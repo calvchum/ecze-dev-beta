87a514fbe5e9eac27eb9d75e808f031e927d6191
Calvin Cheung <calvchum@gmail.com>
2019-06-09 23:12:28 +1000

    programmatically generate pages based on contentful blog content

---
+++ src/templates/blog-post.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default class BlogPostTemplate extends Component {
+  render() {
+    const post = this.props.data.contentfulBlogPost
+    const site = this.props.data.site
+    return (
+      <Layout>
+        <SEO title={`${post.title}`} />
+        <h1>{post.title}</h1>
+        <p>{post.publishDate}</p>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: post.body.childMarkdownRemark.html,
+          }}
+        />
+      </Layout>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishDate(formatString: "MMMM Do, YYYY")
+      heroImage {
+        fluid(maxWidth: 1180, background: "rgb:000000") {
+          ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`
