bd249c29d76f569d3d0bce265514757bcfbca0a6
Calvin Cheung <calvchum@gmail.com>
2019-06-12 19:56:12 +1000

    slight CSS changes to articlePreview.js

--- src/components/articlepreview.js
+++ src/components/articlepreview.js
@@ -9,12 +9,13 @@ class ArticlePreview extends Component {
       <Link to={`/blog/${slug}`} style={{ textDecoration: "none" }}>
         <div
           style={{
+            width: "33%",
             border: "1px solid",
             borderRadius: "4px",
             marginBottom: "10px",
           }}
         >
-          <img src={imgUrl} alt="title" style={{ height: "100px" }} />
+          <img src={imgUrl} alt="title" style={{ width: "100%" }} />
           <li style={{ listStyleType: "none" }}>{title}</li>
         </div>
       </Link>
