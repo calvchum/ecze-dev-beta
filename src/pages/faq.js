2a6b0d0b43a1878492f4c0bf36d738689922a6c9
Calvin Cheung <calvchum@gmail.com>
2019-06-13 23:30:31 +1000

    created FAQ and faqlistItem components

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,29 +1,44 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { StaticQuery, Link, graphql } from "gatsby"
 import Layout from "../components/layout"
+import FaqListItem from "../components/faqListItem"
 import SEO from "../components/seo"
 
 const Faq = () => (
-  <Layout>
-    <SEO title="FAQ" />
-    <h1>FAQs</h1>
-    <p>Products. Allergies. Shipping. Info.</p>
-  </Layout>
-)
-
-export default Faq
-
-export const query = graphql`
-  {
-    allContentfulFaQs {
-      edges {
-        node {
-          question
-          answer {
-            answer
+  <StaticQuery
+    query={graphql`
+      query faqQuestionQuery {
+        allContentfulFaQs {
+          edges {
+            node {
+              question
+              answer {
+                answer
+              }
+            }
           }
         }
       }
-    }
-  }
-`
+    `}
+    render={data => (
+      <>
+        <Layout>
+          <SEO title="FAQ" />
+          <h1>FAQs</h1>
+          <p>Products. Allergies. Shipping. Info.</p>
+          {data.allContentfulFaQs.edges.map((edges, i) => {
+            return (
+              <FaqListItem
+                key={i}
+                question={edges.node.question}
+                answer={edges.node.answer.answer}
+              />
+            )
+          })}
+        </Layout>
+      </>
+    )}
+  />
+)
+
+export default Faq
