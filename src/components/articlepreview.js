674ffebb9132819a8bf847db4a6f01cc0f3290e2
Calvin Cheung <calvchum@gmail.com>
2019-07-05 17:28:21 +0800

    refactored articlepreview to stateless functional component, destructured all the props

--- src/components/articlepreview.js
+++ src/components/articlepreview.js
@@ -1,27 +1,56 @@
-import React, { Component } from "react"
+import React from "react"
 import { Link } from "gatsby"
 
-class ArticlePreview extends Component {
-  render() {
-    const { title, slug, category } = this.props.post.node
-    const imgUrl = this.props.post.node.heroImage.file.url
-    return (
-      <Link to={`/blog/${slug}`} style={{ textDecoration: "none" }}>
-        <div
-          style={{
-            width: "33%",
-            border: "1px solid",
-            borderRadius: "4px",
-            marginBottom: "10px",
-          }}
-        >
-          <img src={imgUrl} alt="title" style={{ width: "100%" }} />
-          <li style={{ listStyleType: "none" }}>{category}</li>
-          <li style={{ listStyleType: "none" }}>{title}</li>
-        </div>
-      </Link>
-    )
-  }
-}
+// class ArticlePreview extends Component {
+//   render() {
+//     const { title, slug, category } = this.props.post.node
+//     const imgUrl = this.props.post.node.heroImage.file.url
+//     return (
+//       <Link to={`/blog/${slug}`} style={{ textDecoration: "none" }}>
+//         <div
+//           style={{
+//             width: "33%",
+//             border: "1px solid",
+//             borderRadius: "4px",
+//             marginBottom: "10px",
+//           }}
+//         >
+//           <img src={imgUrl} alt="title" style={{ width: "100%" }} />
+//           <li style={{ listStyleType: "none" }}>{category}</li>
+//           <li style={{ listStyleType: "none" }}>{title}</li>
+//         </div>
+//       </Link>
+//     )
+//   }
+// }
 
+// export default ArticlePreview
+
+const ArticlePreview = ({
+  post: {
+    node: {
+      slug,
+      category,
+      title,
+      heroImage: {
+        file: { url },
+      },
+    },
+  },
+}) => (
+  <Link to={`/blog/${slug}`} style={{ textDecoration: "none" }}>
+    <div
+      style={{
+        width: "33%",
+        border: "1px solid",
+        borderRadius: "4px",
+        marginBottom: "10px",
+      }}
+    >
+      <img src={url} alt="title" style={{ width: "100%" }} />
+      <li style={{ listStyleType: "none" }}>{category}</li>
+      <li style={{ listStyleType: "none" }}>{title}</li>
+    </div>
+  </Link>
+)
 export default ArticlePreview
