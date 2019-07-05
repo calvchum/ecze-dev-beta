4d857a00556384bb13397c4d2962cf238dc154a5
Calvin Cheung <calvchum@gmail.com>
2019-07-05 17:33:07 +0800

    removed old code

--- src/components/articlepreview.js
+++ src/components/articlepreview.js
@@ -1,31 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 
-// class ArticlePreview extends Component {
-//   render() {
-//     const { title, slug, category } = this.props.post.node
-//     const imgUrl = this.props.post.node.heroImage.file.url
-//     return (
-//       <Link to={`/blog/${slug}`} style={{ textDecoration: "none" }}>
-//         <div
-//           style={{
-//             width: "33%",
-//             border: "1px solid",
-//             borderRadius: "4px",
-//             marginBottom: "10px",
-//           }}
-//         >
-//           <img src={imgUrl} alt="title" style={{ width: "100%" }} />
-//           <li style={{ listStyleType: "none" }}>{category}</li>
-//           <li style={{ listStyleType: "none" }}>{title}</li>
-//         </div>
-//       </Link>
-//     )
-//   }
-// }
-
-// export default ArticlePreview
-
 const ArticlePreview = ({
   post: {
     node: {
