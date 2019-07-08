c271e877f1b67b2ba461c110bc28759d839c654a
Ben Quartermaine <bq@Bens-MacBook-Pro.local>
2019-07-08 11:08:54 +1000

    card structure in place, tweaks to aesthetics need to be made. Should be good to work with for the filter section

--- src/components/articlepreview.js
+++ src/components/articlepreview.js
@@ -1,5 +1,41 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from 'styled-components'
+import { colors, SubheaderText, BodyText } from '../utilities'
+
+const CardWrapper = styled.div`
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.25);
+  // border: 1px solid ${colors.almostWhite}
+`;
+
+const BodyTextUppercase = styled(BodyText)`
+  text-transform: uppercase;
+`;
+
+const BodyTextCapitalized = styled(BodyText)`
+  text-transform: capitalize;
+  font-weight: 500;
+`;
+
+// Link cant be a styled component because it is GATSBY specific, using an object instead
+const linkStyles = {
+  textDecoration: 'none',
+  color: colors.almostBlack,
+  maxWidth: '306px',
+  maxHeight: '406px'
+}
+
+const BlogImage = styled.img`
+  position: relative;
+    float: left;
+    width:  306px;
+    height: 264px;
+    background-position: 50% 50%;
+    background-repeat:   no-repeat;
+    background-size:     cover;
+`;
+
+
 
 const ArticlePreview = ({
   post: {
@@ -13,19 +49,12 @@ const ArticlePreview = ({
     },
   },
 }) => (
-  <Link to={`/blog/${slug}`} style={{ textDecoration: "none" }}>
-    <div
-      style={{
-        width: "33%",
-        border: "1px solid",
-        borderRadius: "4px",
-        marginBottom: "10px",
-      }}
-    >
-      <img src={url} alt="title" style={{ width: "100%" }} />
-      <li style={{ listStyleType: "none" }}>{category}</li>
-      <li style={{ listStyleType: "none" }}>{title}</li>
-    </div>
+  <Link to={`/blog/${slug}`} style={linkStyles}>
+    <CardWrapper>
+      <BlogImage src={url} alt="blog post image" />
+      <BodyTextUppercase>{category}</BodyTextUppercase>
+      <BodyTextCapitalized>{title}</BodyTextCapitalized>
+    </CardWrapper>
   </Link>
 )
 export default ArticlePreview
