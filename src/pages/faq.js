532281db8e89b5a1b5a756168cbf7bb8b284f7b1
Calvin Cheung <calvchum@gmail.com>
2019-07-11 13:16:59 +0800

    refactored static query to page query and stateless to class component

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,44 +1,35 @@
-import React from "react"
+import React, { Component } from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import FaqListItem from "../components/faqListItem"
 import SEO from "../components/seo"
 
-const Faq = () => (
-  <StaticQuery
-    query={graphql`
-      query faqQuestionQuery {
-        allContentfulFaQs {
-          edges {
-            node {
-              question
-              answer {
-                answer
-              }
-            }
+export default class Faq extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="FAQ" />
+        <h1>FAQs</h1>
+        <p>Products. Allergies. Shipping. Info.</p>
+        {this.props.data.allContentfulFaQs.edges.map((edges, i) => {
+          return console.log(edges.node.question, edges.node.answer.answer)
+        })}
+      </Layout>
+    )
+  }
+}
+
+export const query = graphql`
+  query faqQuestionQuery {
+    allContentfulFaQs {
+      edges {
+        node {
+          question
+          answer {
+            answer
           }
         }
       }
-    `}
-    render={data => (
-      <>
-        <Layout>
-          <SEO title="FAQ" />
-          <h1>FAQs</h1>
-          <p>Products. Allergies. Shipping. Info.</p>
-          {data.allContentfulFaQs.edges.map((edges, i) => {
-            return (
-              <FaqListItem
-                key={i}
-                question={edges.node.question}
-                answer={edges.node.answer.answer}
-              />
-            )
-          })}
-        </Layout>
-      </>
-    )}
-  />
-)
-
-export default Faq
+    }
+  }
+`
