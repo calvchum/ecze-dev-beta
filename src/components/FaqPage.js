622ae73ac70c54a1a7a4ce0847f23252a533a2a7
Ben Quartermaine <bq@Bens-MacBook-Pro.local>
2019-07-11 18:32:37 +1000

    FAQ page styled

---
+++ src/components/FaqPage.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import { colors } from '../utilities'
+
+export const FaqHeaderWrapper = styled.div`
+	padding-top: 3em;
+	& h2 {
+		padding: 0.5em 0em 2em 0em;
+	}
+`;
+
+export const FaqWrapper = styled.div`
+	max-width: 960px;
+	margin: 0 auto;
+
+`;
+
+export const QuestionAnswer = styled.div`
+	border-bottom: 1px ${colors.almostBlack} solid;
+	padding: 1em 0em;
+	&:first-child {
+		border-top: 1px ${colors.almostBlack} solid;
+	}
+`;
+
+export const QuestionWrapper = styled.div`
+	padding-bottom: 4em
+`;
+
+
+export const QuestionArrow = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	cursor: pointer;
+	& img {
+		padding: 0em 1.5em 0em 0.5em;
+		margin-bottom: 0px;
+	}
+`;
+
+
--- src/components/faqListItem.js
+++ src/components/faqListItem.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react"
+import { SubheaderText, BodyText } from "../utilities"
+import { QuestionAnswer, QuestionArrow } from "./FaqPage"
+import downArrow from '../assets/icons/down-arrow.svg'
 
 class FaqListItem extends Component {
 	render() {
 		const { question, answer, index } = this.props
 		return (
-			<div onClick={() => this.props.handleClick(index)}>
-				<h3>{question}</h3>
-				<p>{answer}</p>
-			</div>
+			<QuestionAnswer onClick={() => this.props.handleClick(index)}>
+				<QuestionArrow>
+					<SubheaderText>{question}</SubheaderText>
+					<img src={downArrow} alt=""/>
+				</QuestionArrow>
+				<BodyText>{answer}</BodyText>
+			</QuestionAnswer>
 		)
 	}
 }
