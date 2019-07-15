beb8993abd9cb62f6c44c483d9a7060219ffeb88
Calvin Cheung <calvchum@gmail.com>
2019-07-15 15:49:19 +0800

    updated keys

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+	spaceId: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -38,11 +38,11 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     {
-     resolve: `gatsby-plugin-typography`,
-     options: {
-       pathToConfigModule: `src/utilities/Fonts.js`,
-     }
-    }
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utilities/Fonts.js`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
