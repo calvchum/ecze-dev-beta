8ed63262b9c695803e0fce1f474117c92562fcdc
Calvin Cheung <calvchum@gmail.com>
2019-07-16 15:36:24 +0800

    changed the capitalisation of file names

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,8 +10,9 @@ const HeaderWrapper = styled.div`
   grid: 120px / 1fr 1fr;
   align-items: center;
   padding: 0em ${paddingDefaults.pageBorder};
-  // box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
   background: white;
+  z-index: 1;
 `
 
 const LogoWrapper = styled.img`
