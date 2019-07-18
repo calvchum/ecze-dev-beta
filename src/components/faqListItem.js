4a54c3cce897486231a9047cc95c43f17cd98aed
Calvin Cheung <calvchum@gmail.com>
2019-07-18 14:50:48 +0800

    refactored FaqListItem to functional component

--- src/components/faqListItem.js
+++ src/components/faqListItem.js
@@ -1,21 +1,26 @@
-import React, { Component } from "react"
+import React from "react"
 import { SubheaderText, BodyText } from "../utilities"
 import { QuestionAnswer, QuestionArrow } from "./FaqPage"
-import downArrow from '../assets/icons/down-arrow.svg'
+import downArrow from "../assets/icons/down-arrow.svg"
 
-class FaqListItem extends Component {
-	render() {
-		const { question, answer, index } = this.props
-		return (
-			<QuestionAnswer onClick={() => this.props.handleClick(index)}>
-				<QuestionArrow>
-					<SubheaderText>{question}</SubheaderText>
-					<img src={downArrow} alt=""/>
-				</QuestionArrow>
-				<BodyText>{answer}</BodyText>
-			</QuestionAnswer>
-		)
-	}
-}
+// class FaqListItem extends Component {
+// 	render() {
+// 		const { question, answer, index } = this.props
+// 		return (
+// 		)
+// 	}
+// }
+
+// export default FaqListItem
+
+const FaqListItem = ({ index, question, answer, handleClick }) => (
+	<QuestionAnswer onClick={() => handleClick(index)}>
+		<QuestionArrow>
+			<SubheaderText>{question}</SubheaderText>
+			<img src={downArrow} alt="" />
+		</QuestionArrow>
+		<BodyText>{answer}</BodyText>
+	</QuestionAnswer>
+)
 
 export default FaqListItem
