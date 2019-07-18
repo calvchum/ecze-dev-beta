b3ab4d194ec8ef15cfee44e0a6566c330a35b4e2
Calvin Cheung <calvchum@gmail.com>
2019-07-18 14:23:02 +0800

    changed back to normal convention

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-import Header from "./header"
+import Header from "./Header"
 import Footer from "./Footer"
 import SignUpForm from "./SignUpForm"
 import styled from "styled-components"
