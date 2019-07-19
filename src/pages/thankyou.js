89498cb1a3d1031695cb54291a84a8c4e75ee5ba
Calvin Cheung <calvchum@gmail.com>
2019-07-20 00:29:50 +0800

    added a boiletplate thankyou page and pushing to master to test mailchimp in production

---
+++ src/pages/thankyou.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThankYouPage = () => (
+	<Layout>
+		<SEO title="Subscription success!" />
+		<h1>Thanks for subscribing to ECZE</h1>
+		<p>You will now receive email updates</p>
+	</Layout>
+)
+
+export default ThankYouPage
