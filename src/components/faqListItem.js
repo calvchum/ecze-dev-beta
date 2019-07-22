4e7a12e96457a8985eeaba81382744728b5c6683
Calvin Cheung <calvchum@gmail.com>
2019-07-22 14:16:16 +0800

    passed down isClicked prop to faqListItem

--- src/components/faqListItem.js
+++ src/components/faqListItem.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react"
-import { animated, useSpring } from 'react-spring'
+import { animated, useSpring } from "react-spring"
 import { SubheaderText, BodyText } from "../utilities"
 import { QuestionAnswer, QuestionArrow } from "./FaqPage"
 import downArrow from "../assets/icons/down-arrow.svg"
 
-const FaqListItem = ({ index, question, answer, handleClick }) => {
-	const [isQuestionOpen, setQuestionOpen] = useState(false);
-  const arrowAnimation = useSpring({
-    transform: isQuestionOpen ? `rotate(180deg)` : `rotate(0deg)`
-  });
+const FaqListItem = ({ index, question, answer, handleClick, isClicked }) => {
+	const [isQuestionOpen, setQuestionOpen] = useState(false)
+	const arrowAnimation = useSpring({
+		transform: isQuestionOpen ? `rotate(180deg)` : `rotate(0deg)`,
+	})
 
+	console.log(isClicked)
 	return (
-		<QuestionAnswer onClick={() => { handleClick(index); setQuestionOpen(!isQuestionOpen); }}>
+		<QuestionAnswer
+			onClick={() => {
+				handleClick(index)
+				setQuestionOpen(!isQuestionOpen)
+			}}
+		>
 			<QuestionArrow>
 				<SubheaderText>{question}</SubheaderText>
 				<animated.img style={arrowAnimation} src={downArrow} alt="" />
