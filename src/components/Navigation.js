a2dff675348964e630cdcd58decff07f01240088
Calvin Cheung <calvchum@gmail.com>
2019-07-23 09:18:45 +0800

    added some logic that if the link is to get started adds the Underline component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,8 @@ import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 import * as data from "../constants/pageInfo"
-import { colors, NavLinksFont } from "../utilities"
+import { colors, NavLinksFont, lineWidths } from "../utilities"
+import { Underline } from "./Underline"
 
 const ListItem = styled.li`
   ${NavLinksFont}
@@ -18,18 +19,28 @@ const ListItem = styled.li`
     color: ${colors.primary}
     text-decoration: underline;
   }
-`;
+`
 
 const Navigation = ({ props }) => (
   <>
     {data.pageData.map((page, i) => {
-      return (
-        <ListItem key={i}>
-          <Link to={page.link}> 
-            {page.title}
-          </Link>
-        </ListItem> 
-      )
+      if (page.link === "/getstarted") {
+        return (
+          <ListItem key={i}>
+            <Link to={page.link}>
+              <Underline size={lineWidths.ctaUnderline} color={colors.primary}>
+                {page.title}
+              </Underline>
+            </Link>
+          </ListItem>
+        )
+      } else {
+        return (
+          <ListItem key={i}>
+            <Link to={page.link}>{page.title}</Link>
+          </ListItem>
+        )
+      }
     })}
   </>
 )
