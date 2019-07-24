d48459555913d11faaf4b1dc8d8822f40ae3b6a0
Calvin Cheung <calvchum@gmail.com>
2019-07-24 23:01:17 +0800

    fixed cta

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
-import { BodyText, HeaderText, paddingDefaults, media } from '../utilities'
-import { CTAButton } from './Buttons'
+import { BodyText, HeaderText, paddingDefaults, media } from "../utilities"
+import { CTAButton } from "./Buttons"
 
 const HeroBannerWrapper = styled.div`
   display: grid;
@@ -15,28 +15,33 @@ const HeroBannerWrapper = styled.div`
   ${media.med`
     padding: ${paddingDefaults.topBottom} 1em;
   `}
-`;
+`
 
 const HeroBannerText = styled.div`
   max-width: 672px;
-`;
+`
 
 const ButtonWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
   padding-top: 3em;
-`;
+`
 
 const HeroBanner = () => (
   <HeroBannerWrapper>
     <HeroBannerText>
-      <HeaderText>Fed up with eczema? You’ve come to the right place.</HeaderText>
-      <BodyText>We provide simple and holistic remedies to help you overcome your eczema.</BodyText>
+      <HeaderText>
+        Fed up with eczema? You’ve come to the right place.
+      </HeaderText>
+      <BodyText>
+        We provide simple and holistic remedies to help you overcome your
+        eczema.
+      </BodyText>
     </HeroBannerText>
     <ButtonWrapper>
-      <CTAButton cta="Lets get started" />
+      <CTAButton cta="Lets get started" link="getstarted" />
     </ButtonWrapper>
   </HeroBannerWrapper>
 )
 
-export default HeroBanner;
+export default HeroBanner
