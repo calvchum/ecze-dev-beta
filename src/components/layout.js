bedba94f05de35ffd9cb05ed0eebd6c1e916132f
Ben Quartermaine <bq@Bens-MacBook-Pro.local>
2019-07-24 14:04:43 +1000

    forced background: transparent on the CTA button

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,10 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import Header from "./Header"
 import Footer from "./Footer"
 import SignUpForm from "./SignUpForm"
-import styled from "styled-components"
 import "./layout.css"
-
-const MainContainer = styled.div``
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -30,9 +20,9 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <MainContainer>
+        <div>
           <main>{children}</main>
-        </MainContainer>
+        </div>
         <SignUpForm />
         <Footer />
       </>
