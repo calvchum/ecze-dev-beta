df14d3aa7eae952a91e60f8d76e82afc2b92c585
Ben Quartermaine <bq@Bens-MacBook-Pro.local>
2019-08-06 19:21:52 +1000

    pushing Calvs changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -69,6 +69,7 @@ const Footer = ({ path }) => (
         <Navigation path={path} />
       </FooterNavWrapper>
       <ContactEcze>
+      
         <SubheaderText>Get in Touch</SubheaderText>
         <a href="mailto:hello@ecze.co">
           <BodyText
