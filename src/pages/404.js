5e47453e23072b3f4261ba89caa81ec997e95818
Calvin Cheung <calvchum@gmail.com>
2019-08-08 09:38:30 +0800

    added props to layout in 404 and blog-template

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,15 +5,12 @@ import ReactModal from "react-modal"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-	<Layout>
+const NotFoundPage = props => (
+	<Layout props={props}>
 		<SEO title="404: Not found" />
 		<h1>NOT FOUND</h1>
 		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-		<Link to="testmodal" asModal>
-			Home as modal
-		</Link>
-		<ReactModal isOpen={true} />
+		<Link to="/">Home</Link>
 	</Layout>
 )
 
